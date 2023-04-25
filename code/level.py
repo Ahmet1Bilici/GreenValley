56d09195aa5203fb27a544f03b8944b44cce62d8
Ahmet <1yigitbilici@gmail.com>
2023-04-25 16:38:34 -0500

    Add player inventory

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from sprites import Generic, Water, WildFlower, Tree
 from pytmx.util_pygame import load_pygame
 from support import *
 
+
 class Level:
 	def __init__(self):
 
@@ -45,7 +46,13 @@ class Level:
 
 		# Trees
 		for obj in tmx_data.get_layer_by_name('Trees'):
-			Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+			Tree(
+				pos = (obj.x, obj.y),
+				surf = obj.image,
+				groups = [self.all_sprites, self.collision_sprites, self.tree_sprites],
+				name = obj.name,
+				player_add = self.player_add
+			)
 
 		# Wildflowers
 		for obj in tmx_data.get_layer_by_name('Decoration'):
@@ -69,6 +76,9 @@ class Level:
 			surf = pygame.image.load("../graphics/world/ground.png").convert_alpha(),
 			groups = self.all_sprites,
 			z = LAYERS['ground'])
+
+	def player_add(self, item):
+		self.player.item_inventory[item] += 1
 
 	def run(self,dt):
 		self.display_surface.fill('black')
--- code/sprites.py
+++ code/sprites.py
@@ -61,7 +61,7 @@ class Particle(Generic):
 
 
 class Tree(Generic):
-    def __init__(self, pos, surf, groups, name):
+    def __init__(self, pos, surf, groups, name, player_add):
         super().__init__(pos, surf, groups)
 
         # tree attributes
@@ -77,6 +77,8 @@ class Tree(Generic):
         self.apple_sprites = pygame.sprite.Group()
         self.create_fruit()
 
+        self.player_add = player_add
+
     def damage(self):
         # gives damages the tree
         self.health -= 1
@@ -89,6 +91,7 @@ class Tree(Generic):
                 surf = random_apple.image,
                 groups = self.groups()[0],
                 z = LAYERS['fruit'])
+            self.player_add('apple')
             random_apple.kill()
 
     def check_tree_death(self):
@@ -103,6 +106,7 @@ class Tree(Generic):
             self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate(-10, -self.rect.height * 0.6)
             self.alive = False
+            self.player_add('wood')
 
 
     def update(self, dt):

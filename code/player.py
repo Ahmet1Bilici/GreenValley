24b745a30cb8a2c547df7f7382a152cd3600e94f
Ahmet <1yigitbilici@gmail.com>
2023-04-25 12:04:56 -0500

    Add player graphics

--- code/player.py
+++ code/player.py
@@ -1,19 +1,35 @@
 import pygame
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
         # initial setup
-        self.image = pygame.Surface((32,64))
-        self.image.fill('blue')
-        self.rect = self.image.get_rect(center = pos)
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=pos)
 
         # movement attributes
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
+            print(self.animations)
+
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -34,7 +50,7 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
-    def move(self,dt):
+    def move(self, dt):
 
         # normalize vector
         if self.direction.magnitude() > 0:
@@ -47,7 +63,6 @@ class Player(pygame.sprite.Sprite):
         # horizontal movement
         self.pos.x += self.direction.x * self.speed * dt
         self.rect.centerx = self.pos.x
-
 
     def update(self, dt):
         self.input()

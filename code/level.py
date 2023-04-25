88416993321930c86506b05bf57411920e8e9b91
Ahmet <1yigitbilici@gmail.com>
2023-04-25 15:11:11 -0500

    Add object depth

--- code/level.py
+++ code/level.py
@@ -77,7 +77,7 @@ class CameraGroup(pygame.sprite.Group):
 		self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
 
 		for layer in LAYERS.values():
-			for sprite in self.sprites():
+			for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
 				if sprite.z == layer:
 					offset_rect = sprite.rect.copy()
 					offset_rect.center -= self.offset
